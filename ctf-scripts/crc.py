329b9cc8a5739e6977a8887f01144a570df5bded
cocowool <cocowool@gmail.com>
2026-04-03 20:44:25 +0800

    add crc caculator script

---
+++ ctf-scripts/crc.py
@@ -0,0 +1,21 @@
+import os
+import binascii
+import struct
+
+# PNG CRC 
+# png 文件路径
+png_file = "/Users/shiqiang/Downloads/ctf-2026/2.png"
+
+current_crc = 0xcbd6df8a
+
+png_hd = open(png_file, 'rb').read()
+
+for i in range(2000):
+    for j in range(2000):
+        data = png_hd[12:16] + \
+            struct.pack('>i', i) + struct.pack('>i', j) + png_hd[24:29]
+        crc = binascii.crc32(data) & 0xffffffff
+        if crc == current_crc:
+            print(f"crc: {crc}")
+            print(f"i: {i}")
+            print(f"j: {j}")
